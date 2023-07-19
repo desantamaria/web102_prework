859621865039c30aadd2ac397e75fb5ff5949035
desantamaria <santamaria.daniel316@gmail.com>
2023-07-19 16:52:16 -0700

    Complete Challenge 7

--- index.js
+++ index.js
@@ -193,7 +193,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [{name: firstName}, {name: secondName}, ...rest] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const firstGame = document.createElement("h3");
+firstGame.innerHTML = `${firstName}`;
+firstGameContainer.appendChild(firstGame);
 
 // do the same for the runner up item
+const secondGame = document.createElement("h3");
+secondGame.innerHTML = `${secondName}`;
+secondGameContainer.appendChild(secondGame);
